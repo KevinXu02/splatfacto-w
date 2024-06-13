5a5415868512891ae647853af769e9d27e5068bb
Congrong Xu <xucr@berkeley.edu>
2024-06-13 18:58:47 +0800

    first commit

--- method_template/splatfactow_config.py
+++ method_template/splatfactow_config.py
@@ -60,3 +60,62 @@ method_template = MethodSpecification(
     ),
     description="Nerfstudio method template.",
 )
+method_configs["splatfacto-w"] = TrainerConfig(
+    method_name="splatfacto-w",
+    steps_per_eval_image=1000000,
+    steps_per_eval_batch=0,
+    steps_per_save=2000,
+    steps_per_eval_all_images=1000000,
+    max_num_iterations=65000,
+    mixed_precision=False,
+    pipeline=VanillaPipelineConfig(
+        datamanager=FullImageDatamanagerConfig(
+            dataparser=PhototourismDataParserConfig(),
+            cache_images_type="uint8",
+        ),
+        model=SplatfactoWModelConfig(),
+    ),
+    optimizers={
+        "means": {
+            "optimizer": AdamOptimizerConfig(lr=1.6e-5, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(
+                lr_final=1.6e-7,
+                max_steps=30000,
+            ),
+        },
+        "appearance_features": {
+            "optimizer": AdamOptimizerConfig(lr=0.02, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(
+                lr_final=1e-3,
+                max_steps=40000,
+            ),
+        },
+        "opacities": {
+            "optimizer": AdamOptimizerConfig(lr=0.03, eps=1e-15),
+            "scheduler": None,
+        },
+        "scales": {
+            "optimizer": AdamOptimizerConfig(lr=0.005, eps=1e-15),
+            "scheduler": None,
+        },
+        "quats": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
+        "camera_opt": {
+            "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=5e-5, max_steps=30000),
+        },
+        "field_background": {
+            "optimizer": AdamOptimizerConfig(lr=2e-3, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=30000),
+        },
+        "appearance_model": {
+            "optimizer": AdamOptimizerConfig(lr=2e-3, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=30000),
+        },
+        "appearance_embed": {
+            "optimizer": AdamOptimizerConfig(lr=0.02, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=3e-4, max_steps=40000),
+        },
+    },
+    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+    vis="viewer",
+)
