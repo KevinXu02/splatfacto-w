ba2f657b2fe6e9e135839a8ce7ecbffba63c6893
xucr <xucr@shanghaitech.edu.cn>
2024-06-14 15:40:10 +0800

    chore: update code formatting and remove unused imports

--- splatfactow/phototourism_dataparser.py
+++ splatfactow/phototourism_dataparser.py
@@ -26,13 +26,20 @@ import torch
 
 from nerfstudio.cameras import camera_utils
 from nerfstudio.cameras.cameras import Cameras, CameraType
-from nerfstudio.data.dataparsers.base_dataparser import DataParser, DataParserConfig, DataparserOutputs
+from nerfstudio.data.dataparsers.base_dataparser import (
+    DataParser,
+    DataParserConfig,
+    DataparserOutputs,
+)
 from nerfstudio.data.scene_box import SceneBox
 from nerfstudio.data.utils import colmap_parsing_utils as colmap_utils
 
 # TODO(1480) use pycolmap instead of colmap_parsing_utils
 # import pycolmap
-from nerfstudio.data.utils.colmap_parsing_utils import read_cameras_binary, read_images_binary
+from nerfstudio.data.utils.colmap_parsing_utils import (
+    read_cameras_binary,
+    read_images_binary,
+)
 from nerfstudio.utils.rich_utils import CONSOLE
 
 
@@ -48,7 +55,7 @@ class PhototourismDataParserConfig(DataParserConfig):
     """How much to scale the camera origins by."""
     alpha_color: str = "white"
     """alpha color of background"""
-    train_split_fraction: float = 0.9
+    train_split_fraction: float = 0.95
     """The fraction of images to use for training. The remaining images are for eval."""
     scene_scale: float = 1.0
     """How much to scale the region of interest by."""
@@ -59,7 +66,6 @@ class PhototourismDataParserConfig(DataParserConfig):
     auto_scale_poses: bool = True
     """Whether to automatically scale the poses to fit in +/- 1 bounding box."""
     colmap_path: Path = Path("dense/sparse")
-    """The relative path to the colmap reconstruction."""
     load_3D_points: bool = True
     """Whether to load the 3D points from the colmap reconstruction. This is helpful for Gaussian splatting and
     generally unused otherwise, but it's typically harmless so we default to True."""
@@ -82,13 +88,15 @@ class Phototourism(DataParser):
     def __init__(self, config: PhototourismDataParserConfig):
         super().__init__(config=config)
         self.data: Path = config.data
-        self._downscale_factor = None
+        self.i_eval = None
 
     def _generate_dataparser_outputs(self, split="train"):
         image_filenames = []
         poses = []
         colmap_path = self.data / self.config.colmap_path
-        with CONSOLE.status(f"[bold green]Reading phototourism images and poses for {split} split...") as _:
+        with CONSOLE.status(
+            f"[bold green]Reading phototourism images and poses for {split} split..."
+        ) as _:
             # TODO(1480) use pycolmap
             # recon = pycolmap.Reconstruction(self.data / "dense" / "sparse")
             # cams = recon.cameras
@@ -110,9 +118,14 @@ class Phototourism(DataParser):
         for _id, cam in cams.items():
             img = imgs[_id]
 
-            assert cam.model == "PINHOLE", "Only pinhole (perspective) camera model is supported at the moment"
+            assert (
+                cam.model == "PINHOLE"
+            ), "Only pinhole (perspective) camera model is supported at the moment"
 
-            pose = torch.cat([torch.tensor(img.qvec2rotmat()), torch.tensor(img.tvec.reshape(3, 1))], dim=1)
+            pose = torch.cat(
+                [torch.tensor(img.qvec2rotmat()), torch.tensor(img.tvec.reshape(3, 1))],
+                dim=1,
+            )
             pose = torch.cat([pose, torch.tensor([[0.0, 0.0, 0.0, 1.0]])], dim=0)
             poses.append(torch.linalg.inv(pose))
             fxs.append(torch.tensor(cam.params[0]))
@@ -138,19 +151,23 @@ class Phototourism(DataParser):
             0, num_images - 1, num_train_images, dtype=int
         )  # equally spaced training images starting and ending at 0 and num_images-1
         i_eval = np.setdiff1d(i_all, i_train)  # eval images are the remaining images
-        i_all = torch.tensor(i_all)
+        # change ievel to list
+        self.i_eval = i_eval.tolist()
+        i_all = torch.tensor(i_all, dtype=torch.long)
         i_train = torch.tensor(i_train, dtype=torch.long)
         i_eval = torch.tensor(i_eval, dtype=torch.long)
         assert len(i_eval) == num_eval_images
         if split == "train":
-            indices = i_train
+            indices = i_all
         elif split in ["val", "test"]:
             indices = i_eval
         else:
             raise ValueError(f"Unknown dataparser split {split}")
 
         poses, transform_matrix = camera_utils.auto_orient_and_center_poses(
-            poses, method=self.config.orientation_method, center_method=self.config.center_method
+            poses,
+            method=self.config.orientation_method,
+            center_method=self.config.center_method,
         )
 
         # Scale poses
@@ -166,7 +183,11 @@ class Phototourism(DataParser):
         aabb_scale = self.config.scene_scale
         scene_box = SceneBox(
             aabb=torch.tensor(
-                [[-aabb_scale, -aabb_scale, -aabb_scale], [aabb_scale, aabb_scale, aabb_scale]], dtype=torch.float32
+                [
+                    [-aabb_scale, -aabb_scale, -aabb_scale],
+                    [aabb_scale, aabb_scale, aabb_scale],
+                ],
+                dtype=torch.float32,
             )
         )
 
@@ -178,12 +199,13 @@ class Phototourism(DataParser):
             cy=cys,
             camera_type=CameraType.PERSPECTIVE,
         )
-
         cameras = cameras[indices]
         image_filenames = [image_filenames[i] for i in indices]
         metadata = {}
         if self.config.load_3D_points:
-            metadata.update(self._load_3D_points(colmap_path, transform_matrix, scale_factor))
+            metadata.update(
+                self._load_3D_points(colmap_path, transform_matrix, scale_factor)
+            )
         assert len(cameras) == len(image_filenames)
 
         dataparser_outputs = DataparserOutputs(
@@ -197,14 +219,24 @@ class Phototourism(DataParser):
 
         return dataparser_outputs
 
-    def _load_3D_points(self, colmap_path: Path, transform_matrix: torch.Tensor, scale_factor: float):
+    def _load_3D_points(
+        self, colmap_path: Path, transform_matrix: torch.Tensor, scale_factor: float
+    ):
         if (colmap_path / "points3D.bin").exists():
-            colmap_points = colmap_utils.read_points3D_binary(colmap_path / "points3D.bin")
+            colmap_points = colmap_utils.read_points3D_binary(
+                colmap_path / "points3D.bin"
+            )
         elif (colmap_path / "points3D.txt").exists():
-            colmap_points = colmap_utils.read_points3D_text(colmap_path / "points3D.txt")
+            colmap_points = colmap_utils.read_points3D_text(
+                colmap_path / "points3D.txt"
+            )
         else:
-            raise ValueError(f"Could not find points3D.txt or points3D.bin in {colmap_path}")
-        points3D = torch.from_numpy(np.array([p.xyz for p in colmap_points.values()], dtype=np.float32))
+            raise ValueError(
+                f"Could not find points3D.txt or points3D.bin in {colmap_path}"
+            )
+        points3D = torch.from_numpy(
+            np.array([p.xyz for p in colmap_points.values()], dtype=np.float32)
+        )
         points3D = (
             torch.cat(
                 (
@@ -218,25 +250,39 @@ class Phototourism(DataParser):
         points3D *= scale_factor
 
         # Load point colours
-        points3D_rgb = torch.from_numpy(np.array([p.rgb for p in colmap_points.values()], dtype=np.uint8))
-        points3D_num_points = torch.tensor([len(p.image_ids) for p in colmap_points.values()], dtype=torch.int64)
+        points3D_rgb = torch.from_numpy(
+            np.array([p.rgb for p in colmap_points.values()], dtype=np.uint8)
+        )
+        points3D_num_points = torch.tensor(
+            [len(p.image_ids) for p in colmap_points.values()], dtype=torch.int64
+        )
         out = {
             "points3D_xyz": points3D,
             "points3D_rgb": points3D_rgb,
-            "points3D_error": torch.from_numpy(np.array([p.error for p in colmap_points.values()], dtype=np.float32)),
+            "points3D_error": torch.from_numpy(
+                np.array([p.error for p in colmap_points.values()], dtype=np.float32)
+            ),
             "points3D_num_points2D": points3D_num_points,
         }
         if self.config.max_2D_matches_per_3D_point != 0:
             if (colmap_path / "images.txt").exists():
-                im_id_to_image = colmap_utils.read_images_text(colmap_path / "images.txt")
+                im_id_to_image = colmap_utils.read_images_text(
+                    colmap_path / "images.txt"
+                )
             elif (colmap_path / "images.bin").exists():
-                im_id_to_image = colmap_utils.read_images_binary(colmap_path / "images.bin")
+                im_id_to_image = colmap_utils.read_images_binary(
+                    colmap_path / "images.bin"
+                )
             else:
-                raise ValueError(f"Could not find images.txt or images.bin in {colmap_path}")
+                raise ValueError(
+                    f"Could not find images.txt or images.bin in {colmap_path}"
+                )
             downscale_factor = self._downscale_factor
             max_num_points = int(torch.max(points3D_num_points).item())
             if self.config.max_2D_matches_per_3D_point > 0:
-                max_num_points = min(max_num_points, self.config.max_2D_matches_per_3D_point)
+                max_num_points = min(
+                    max_num_points, self.config.max_2D_matches_per_3D_point
+                )
             points3D_image_ids = []
             points3D_image_xy = []
             for p in colmap_points.values():
@@ -244,21 +290,48 @@ class Phototourism(DataParser):
                 nxy_ids = np.array(p.point2D_idxs, dtype=np.int32)
                 if self.config.max_2D_matches_per_3D_point != -1:
                     # Randomly sample 2D matches
-                    idxs = np.argsort(p.error)[: self.config.max_2D_matches_per_3D_point]
+                    idxs = np.argsort(p.error)[
+                        : self.config.max_2D_matches_per_3D_point
+                    ]
                     nids = nids[idxs]
                     nxy_ids = nxy_ids[idxs]
-                nxy = [im_id_to_image[im_id].xys[pt_idx] for im_id, pt_idx in zip(nids, nxy_ids)]
+                nxy = [
+                    im_id_to_image[im_id].xys[pt_idx]
+                    for im_id, pt_idx in zip(nids, nxy_ids)
+                ]
                 nxy = torch.from_numpy(np.stack(nxy).astype(np.float32))
                 nids = torch.from_numpy(nids)
                 assert len(nids.shape) == 1
                 assert len(nxy.shape) == 2
                 points3D_image_ids.append(
-                    torch.cat((nids, torch.full((max_num_points - len(nids),), -1, dtype=torch.int64)))
+                    torch.cat(
+                        (
+                            nids,
+                            torch.full(
+                                (max_num_points - len(nids),), -1, dtype=torch.int64
+                            ),
+                        )
+                    )
                 )
                 points3D_image_xy.append(
-                    torch.cat((nxy, torch.full((max_num_points - len(nxy), nxy.shape[-1]), 0, dtype=torch.float32)))
+                    torch.cat(
+                        (
+                            nxy,
+                            torch.full(
+                                (max_num_points - len(nxy), nxy.shape[-1]),
+                                0,
+                                dtype=torch.float32,
+                            ),
+                        )
+                    )
                     / downscale_factor
                 )
             out["points3D_image_ids"] = torch.stack(points3D_image_ids, dim=0)
             out["points3D_points2D_xy"] = torch.stack(points3D_image_xy, dim=0)
         return out
+
+    def check_in_eval(self, idx):
+        return idx in self.i_eval
+
+    def find_eval_idx(self, idx):
+        return self.i_eval[idx]
